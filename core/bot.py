7c63ad8bca729cf77550aaceca58cc1897a5cde1
nepeat <nepeat@users.noreply.github.com>
2016-03-14 01:14:50 -0800

    Initial commit.

---
+++ core/bot.py
@@ -0,0 +1,42 @@
+import discord
+import asyncio
+import os
+import re
+import redis
+
+redis_client = redis.StrictRedis(
+    host=os.environ.get("REDIS_HOST", "127.0.0.1"),
+    port=os.environ.get("REDIS_PORT", 6379),
+    db=os.environ.get("REDIS_DB", 0)
+)
+
+client = discord.Client()
+
+commands = {}
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+    load_commands()
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('!test'):
+        counter = 0
+        tmp = await client.send_message(message.channel, 'Calculating messages...')
+        async for log in client.logs_from(message.channel, limit=100):
+            if log.author == message.author:
+                counter += 1
+
+        await client.edit_message(tmp, 'You have {} messages.'.format(counter))
+    elif message.content.startswith('!sleep'):
+        await asyncio.sleep(5)
+        await client.send_message(message.channel, 'Done sleeping')
+
+client.run(
+    os.environ["DISCORD_USERNAME"],
+    os.environ["DISCORD_PASSWORD"]
+)
