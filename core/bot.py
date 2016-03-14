d8fc56091a24ba18107e021c2004cb32fe337bf0
nepeat <nepeat@users.noreply.github.com>
2016-03-14 03:24:03 -0800

    what is this hax, lol

--- core/bot.py
+++ core/bot.py
@@ -1,51 +1,56 @@
 import discord
 import asyncio
 import os
+import sys
 import re
-import redis
+import asyncio_redis
 import json
-
-redis_client = redis.StrictRedis(
-    host=os.environ.get("REDIS_HOST", "127.0.0.1"),
-    port=os.environ.get("REDIS_PORT", 6379),
-    db=os.environ.get("REDIS_DB", 0)
-)
 
 client = discord.Client()
 
 commands = {}
 
-def load_commands(ps_message=None):
+async def get_redis():
+    return await asyncio_redis.Connection.create(
+        host=os.environ.get("REDIS_HOST", "127.0.0.1"),
+        port=os.environ.get("REDIS_PORT", 6379),
+        db=os.environ.get("REDIS_DB", 0),
+        encoder=asyncio_redis.encoders.UTF8Encoder()
+    )
+
+async def load_commands():
+    redis_client = await get_redis()
     try:
         commands.clear()
-        commands.update(json.loads(redis_client.get("bot:commands")))
+        rawcommands = await redis_client.get("bot:commands")
+        commands.update(json.loads(rawcommands))
     except (TypeError, ValueError):
         pass
 
-async def on_pubsub():
+    print("Loaded %s commands." % (len(commands)))
+
+async def on_pubsub(ps_message):
+    print(ps_message)
+
     try:
-        data = json.loads(message['data'])
+        data = json.loads(ps_message.value)
     except ValueError:
-        print("ValueError trying to parse '%s'" % (message["data"]))
+        print("ValueError trying to parse '%s'" % (ps_message.value))
         return
 
     if "action" not in data:
         return
 
     if data["action"] == "reload":
-        load_commands()
+        await load_commands()
     elif data["action"] == "say":
         if "channel" not in data or "text" not in data:
             return
 
-        channel = await client.get_channel(data["channel"])
+        channel = client.get_channel(str(data["channel"]))
 
         if channel:
             await client.send_message(channel, data["text"])
-    else:
-        pass
-
-    return
 
 @client.event
 async def on_ready():
@@ -53,7 +58,18 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
-    load_commands()
+    await load_commands()
+
+    redis_client = await get_redis()
+    subscriber = await redis_client.start_subscribe()
+    await subscriber.psubscribe(["core:*"])
+
+    while True:
+        reply = await subscriber.next_published()
+        await on_pubsub(reply)
+
+    # When finished, close the connection.
+    connection.close()
 
 @client.event
 async def on_message(message):
@@ -73,32 +89,19 @@ async def on_message(message):
             await client.send_message(message.channel, output.strip())
     elif message.content.startswith('!ping'):
         await client.send_message(message.channel, 'Bot is alive.')
+    elif message.content.startswith("!debug"):
+        await client.send_message(message.channel, "<@{uid}> You are UID {uid}. This channel's ID is {cid}".format(
+            uid=message.author.id,
+            cid=message.channel.id
+        ))
 
 if "example.com" in os.environ["DISCORD_USERNAME"]:
     raise ValueError("Please set your DISCORD_USERNAME to a valid username.")
 
 if __name__ == "__main__":
-    # Pubsub
-    ps = redis_client.pubsub(ignore_subscribe_messages=True)
-    ps.psubscribe(**{"core:*": on_pubsub})
-    psthread = ps.run_in_thread(sleep_time=0.01)
+    print("Bot started!")
 
-    try:
-        client.loop.run_until_complete(client.start(
-            os.environ["DISCORD_USERNAME"],
-            os.environ["DISCORD_PASSWORD"]
-        ))
-    except KeyboardInterrupt:
-        print("Stoping bot.")
-        client.loop.run_until_complete(client.logout())
-        pending = asyncio.Task.all_tasks()
-        gathered = asyncio.gather(*pending)
-        try:
-            gathered.cancel()
-            client.loop.run_forever()
-            gathered.exception()
-        except:
-            pass
-    finally:
-        client.loop.close()
-        psthread.stop()
+    client.run(
+        os.environ["DISCORD_USERNAME"],
+        os.environ["DISCORD_PASSWORD"]
+    )
