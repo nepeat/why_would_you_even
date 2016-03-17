51a6310939e6eb4b63e5daf96281eb95abad0fff
nepeat <nepeat@users.noreply.github.com>
2016-03-17 06:15:17 -0800

    Commands to add/remove admins.

--- basecommands/basecommands/commands/admin.py
+++ basecommands/basecommands/commands/admin.py
@@ -1,8 +1,57 @@
 import re
+import random
 
 from basecommands.decorators import command
 from basecommands.exceptions import Message
 from basecommands.backends import redis
+
+user_regex = re.compile(r"<@(\d+)>")
+sudoku = [
+    "https://i.imgur.com/PmJ7pOB.jpg",
+    "https://i.imgur.com/BYc65jG.png",
+    "**ಠ_ಠ**"
+]
+
+
+@command("add_admin",
+    help="Adds an admin to the admin list.",
+    admin=True
+)
+def add_admin(data=None):
+    users = user_regex.search(data["message"]["content"])
+
+    if not users:
+        raise Message("User could not be matched.")
+
+    user = users.group(1)
+    redis.sadd("bot:admins", user)
+
+    raise Message("<@%s> Added <@%s> to the admin list." % (
+        data["author"]["id"],
+        user
+    ))
+
+
+@command("remove_admin",
+    help="Removes an admin from the admin list.",
+    admin=True
+)
+def remove_admin(data=None):
+    users = user_regex.search(data["message"]["content"])
+
+    if not users:
+        raise Message("User could not be matched.")
+
+    user = users.group(1)
+    if user == data["author"]["id"]:
+        raise Message(random.choice(sudoku))
+
+    redis.srem("bot:admins", user)
+
+    raise Message("<@%s> Removed <@%s> to the admin list." % (
+        data["author"]["id"],
+        user
+    ))
 
 
 @command("start_notifications",
