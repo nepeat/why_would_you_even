70ba8e9a9f5019f81fe043dab10729239f795367
nepeat <nepeat@users.noreply.github.com>
2016-03-17 06:15:26 -0800

    coder is dumb

--- basecommands/basecommands/decorators.py
+++ basecommands/basecommands/decorators.py
@@ -3,6 +3,9 @@ from basecommands import bot_commands
 
 def command(name, help="", admin=False):
     def decorate(f):
+        if name in bot_commands:
+            raise Exception("ur dumb. (duped bot_commands name)")
+
         bot_commands[name] = {
             "f": f,
             "admin": admin,
